b2cc927ae4c98c229d546d705230ed9ac5fc4b18
MahanRahmati <mahan.rahmati@protonmail.com>
2023-05-05 12:58:58 +0330

    feat: Add text styles

---
+++ lib/src/constants/text_styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  TextStyle displayLarge = const TextStyle(
+    fontSize: 57,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.25,
+    height: 1.12,
+    textBaseline: TextBaseline.alphabetic,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+}
