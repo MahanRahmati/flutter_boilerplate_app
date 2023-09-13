c693c36a3302ce47762e76318834e42a58ca96c2
MahanRahmati <mahan.rahmati@protonmail.com>
2023-09-13 09:47:18 +0330

    feat: update for 3.13

--- lib/src/providers/global_providers.dart
+++ lib/src/providers/global_providers.dart
@@ -6,6 +6,6 @@ import '/src/providers/theme_mode_provider.dart';
 class GlobalProviders {
   GlobalProviders._();
   static List<SingleChildWidget> providers = <SingleChildWidget>[
-    ListenableProvider<ThemeModeProvider>.value(value: ThemeModeProvider())
+    ListenableProvider<ThemeModeProvider>.value(value: ThemeModeProvider()),
   ];
 }
